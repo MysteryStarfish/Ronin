b37e6e73a545091d5848d037e8322bba22b7fea2
MysteryStarfish <paulhsu1016@gmail.com>
2026-01-13 16:37:59 +0800

    fix(extension): 🐛 fix reversed rotation direction

--- Assets/_Project/Scripts/Core/Utilities/Vector2Extensions.cs
+++ Assets/_Project/Scripts/Core/Utilities/Vector2Extensions.cs
@@ -7,7 +7,7 @@ namespace Ronin.Core
         /// <summary>
         /// 從 form 到 to 逆時針轉了幾度 (0~360)
         /// </summary>
-        public static float ClockwiseAngle(this Vector2 from, Vector2 to)
+        public static float CounterclockwiseAngle(this Vector2 from, Vector2 to)
         {
             var angle = Vector2.SignedAngle(from, to);
             if (angle < 0) return angle + 360;
@@ -17,9 +17,9 @@ namespace Ronin.Core
         /// <summary>
         /// 從 form 到 to 順時針轉了幾度 (0~360)
         /// </summary>
-        public static float CounterclockwiseAngle(this Vector2 from, Vector2 to)
+        public static float ClockwiseAngle(this Vector2 from, Vector2 to)
         {
-            return ClockwiseAngle(to, from);
+            return CounterclockwiseAngle(to, from);
         }
     }
 }
